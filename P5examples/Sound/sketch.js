e074c332de07882025cfb8ed9101758f309ef782
Nick Puckett <nick@altnresearch.com>
2018-09-28 11:59:07 -0400

    c update

--- P5examples/Sound/sketch.js
+++ P5examples/Sound/sketch.js
@@ -1,26 +1,35 @@
+<<<<<<< HEAD
+
+=======
 /*
 Creation & Computation - Kate Hartman
 Sound Example
 */
 
 var rooster;
+>>>>>>> aa06f03edad8f78e2e2c728060fd90cbae21c2f8
 var survive;
 
 
 function preload() {
-  rooster = loadSound("rooster.mp3");
+  
   survive = loadSound("survive.mp3");
 }
 
 function setup() {
-  rooster.play();
+  getAudioContext().resume();
+ 
   createCanvas(400, 400);
 }
 
 function draw() {
   
   if(mouseX < 200){
+    console.log(survive.isPlaying);
+    if (survive.isPlaying() == 0) 
+    {
     survive.play();
+    }
   }else{
     survive.pause();
   }
--- P5examples/Video/sketch.js
+++ P5examples/Video/sketch.js
@@ -1,14 +1,18 @@
+<<<<<<< HEAD:P5examples/videoplay/sketch.js
+=======
 /*
 Creation & Computation - Kate Hartman
 Video Example
 */
 
+>>>>>>> aa06f03edad8f78e2e2c728060fd90cbae21c2f8:P5examples/Video/sketch.js
 var playing = false;
 var fingers;
 var button;
 
 
 function setup() {
+  getAudioContext().resume();
   // specify multiple formats for different browsers
   fingers = createVideo(['GoGoGloves.mp4']);
   button = createButton('play');
